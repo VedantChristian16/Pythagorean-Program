21bbd720d7e4413cc4d09acbcd7ae6970a49fdcc
VedantChristian16 <55919296+VedantChristian16@users.noreply.github.com>
2022-09-23 15:32:37 +0100

    Update Pythagoras' Theorem.py

--- Pythagoras' Theorem.py
+++ Pythagoras' Theorem.py
@@ -4,14 +4,10 @@ Created on 11 / 10 / 2019
 '''
 
 import math
-import time
 
 def Theorem():
     WhichSide = input("Which side are you trying to calculate, the Hypotenuse(H) or an other side(O) ?")
     if WhichSide == "H":
-        time.sleep(2)
-        print("Ok...")
-        time.sleep(1)
         a = input("What is the length of one of the sides in the triangle ?")
         b = input("What is the length of the other side in the triangle ?")
         c = int(a)**2
@@ -20,7 +16,6 @@ def Theorem():
         f = int(e)**(0.5)
         print("The length of the Hypotenuse is",f,"cm")
     elif WhichSide == "O":
-        print("Ok...")
         a = input("What is the length of the Hypotenuse in the triangle ?")
         b = input("What is the length of the other side in the triangle ?")
         c = int(a) * int(a)
@@ -30,5 +25,5 @@ def Theorem():
         print("The length of the other side is",int(f),"cm")
     else:
         exit(0)
-Theorem()
-input("")
+
+return Theorem()
